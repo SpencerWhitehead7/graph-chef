1a6478bee8da1a57683dbae740c302aa05412a70
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2022-01-23 02:26:01 -0500

    res: MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import { CssBaseline } from "@mui/material"
 import React from "react"
 import ReactDOM from "react-dom"
 
 import App from "./App"
-import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
