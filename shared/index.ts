97871daf6c12cdf92c639ef89527f64733f7ffdd
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2022-10-02 21:36:10 -0400

    feat: recipe as graph

--- shared/index.ts
+++ shared/index.ts
@@ -29,7 +29,83 @@ export type RecipeListed = {
   steps: string[]
 }
 
+export type RecipeFlowed = {
+  nodes: NodeFlowed[]
+  edges: EdgeFlowed[]
+}
+
+type NodeFlowed = {
+  id: string
+  data: {
+    inputs: string[]
+    action: string
+    output: string
+  }
+  position: Position
+  type: undefined | "input" | "output"
+}
+
+type EdgeFlowed = {
+  id: string
+  source: string
+  target: string
+  markerEnd: {
+    type: string
+    color: string
+    strokeWidth: string
+  }
+}
+
 export const serializedToListed = ({ data: { nodes } }: RecipeSerialized): RecipeListed => ({
   ingredients: nodes.filter((n) => !Boolean(n.action)).map((n) => n.output),
   steps: nodes.filter((n) => Boolean(n.action)).map((n) => n.action),
 })
+
+export const serializedToFlowed = ({
+  data: { nodes, edges, positions },
+}: RecipeSerialized): RecipeFlowed => {
+  const idsToNodes = nodes.reduce<Record<number, NodeSerialized>>((acc, node) => {
+    acc[node.id] = node
+    return acc
+  }, {})
+
+  const idsToInputIds = edges.reduce<Record<number, number[]>>((acc, edge) => {
+    acc[edge.o] ??= []
+    acc[edge.o]!.push(edge.i)
+    return acc
+  }, {})
+  const idsToOutputIds = edges.reduce<Record<number, number[]>>((acc, edge) => {
+    acc[edge.i] ??= []
+    acc[edge.i]!.push(edge.o)
+    return acc
+  }, {})
+
+  return {
+    nodes: nodes
+      .filter((n) => Boolean(n.action))
+      .map((n) => ({
+        id: String(n.id),
+        data: {
+          inputs: idsToInputIds[n.id]!.map((id) => idsToNodes[id]!.output),
+          action: n.action,
+          output: n.output,
+        },
+        type: !idsToOutputIds[n.id]
+          ? "output"
+          : idsToInputIds[n.id]!.every((id) => !Boolean(idsToNodes[id]!.action))
+          ? "input"
+          : undefined,
+        position: positions[n.id]!,
+      })),
+    edges: edges.map((e) => ({
+      id: `${e.i}-${e.o}`,
+      source: String(e.i),
+      target: String(e.o),
+      markerEnd: {
+        type: "arrowclosed",
+        color: "deepskyblue",
+        strokeWidth: "1px",
+      },
+    })),
+  }
+}
---
+++ static-site/src/components/GraphView.tsx
@@ -0,0 +1,42 @@
+import "preact"
+import ReactFlow, { Controls, Edge, Node } from "reactflow"
+
+import type { RecipeFlowed } from "../../../shared"
+
+type Props = {
+  recipe: RecipeFlowed
+}
+
+const GraphView = ({ recipe }: Props) => (
+  <ReactFlow
+    nodes={
+      recipe.nodes.map((n) => ({
+        ...n,
+        data: { label: <IngredientNode inputs={n.data.inputs} action={n.data.action} /> },
+      })) as unknown as Node[]
+    }
+    edges={recipe.edges as unknown as Edge[]}
+    fitView
+    attributionPosition="top-right"
+  >
+    <Controls />
+  </ReactFlow>
+)
+
+type IngredientNodeProps = {
+  inputs: string[]
+  action: string
+}
+
+const IngredientNode = ({ inputs, action }: IngredientNodeProps) => (
+  <div className="ingredientNode">
+    <ul className="ingredientNode__inputs">
+      {inputs.map((input) => (
+        <li key={input}>{input}</li>
+      ))}
+    </ul>
+    <span className="ingredientNode__action">{action}</span>
+  </div>
+)
+
+export default GraphView
